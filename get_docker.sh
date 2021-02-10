ce5d3340521b1983395c3e499c6215174ccf079a
Negash <i@negash.ru>
2021-02-10 16:00:37 +0500

    Fix get docker and add helm and image prapare

--- get_docker.sh
+++ get_docker.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-mkdir -r /hostfs/tmp/docker/
-chmod 777 -R /hostfs/tmp/docker/
-wget -O /hostfs/tmp/docker/install.sh $INSTALL_SH_URL
-wget -O /hostfs/tmp/docker/docker.bash $INSTALL_BASH_URL
-wget -O /hostfs/tmp/docker/docker.tgz "https://download.docker.com/linux/static/stable/$ARCH/docker-$DOCKER_VERSION.tgz"
-chmod 777 -R /hostfs/tmp/docker/
+mkdir -p /tmp/docker/
+wget -O /tmp/docker/install.sh $INSTALL_SH_URL
+wget -O /tmp/docker/docker.bash $INSTALL_BASH_URL
+wget -O /tmp/docker/docker.tgz "https://download.docker.com/linux/static/stable/$DOCKER_ARCH/docker-$DOCKER_VERSION.tgz"
+chmod 777 -R /tmp/docker/
---
+++ prepare_offline_images.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+mkdir -p /tmp/rancher/
+cd /tmp/rancher/
+wget "https://github.com/rancher/rancher/releases/download/$RANCHER_VERSION/rancher-images.txt"
+wget "https://github.com/rancher/rancher/releases/download/$RANCHER_VERSION/rancher-load-images.sh"
+wget "https://github.com/rancher/rancher/releases/download/$RANCHER_VERSION/rancher-save-images.sh"
+
