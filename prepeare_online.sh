b9d48bbd1ba9977144af66123d854fc8627d19c7
Negash <i@negash.ru>
2021-02-10 16:36:15 +0500

    Add all images to rancher-images.txt

--- prepeare_online.sh
+++ prepeare_online.sh
@@ -3,8 +3,24 @@ apk add --update bash grep
 
 sh /prepare_offline_images.sh
 
-sh /get_docker.sh
-docker build -t bastion-static -f /Dockerfile.bastion /tmp/docker
+# save all rancher images
+bash /tmp/ranche/rancher-save-images.sh
+
+echo "install helm repos"
+sh /get_helm.sh
+
 echo bastion-static >> /tmp/rancher/rancher-images.txt
 
-sh /get_helm.sh
+echo "add rke and huperkube versions"
+echo rancher/rke-tools:$RKE_TOOLS_VERSION >> /tmp/rancher/rancher-images.txt
+echo rancher/hyperkube:$HUPERKUBE_VERSION >> /tmp/rancher/rancher-images.txt
+
+echo "remove dublicate from rancher-images.txt"
+sort -u /tmp/rancher/rancher-images.txt -o /tmp/rancher/rancher-images.txt
+sort /tmp/rancher/rancher-images.txt | uniq -u > /tmp/rancher/rancher-images.txt
+
+echo "install docker"
+sh /get_docker.sh
+docker build -t bastion-static -f /Dockerfile.bastion /tmp/docker
+
+cat /tmp/rancher/rancher-images.txt
