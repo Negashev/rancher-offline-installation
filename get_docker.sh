f57b600cbbdbcb31f15d93bf8ff10b72346a401f
Negash <i@negash.ru>
2021-02-10 17:58:17 +0500

    Don't download docker if exists

--- get_docker.sh
+++ get_docker.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 mkdir -p /tmp/docker/
-wget -O /tmp/docker/install.sh $INSTALL_SH_URL
-wget -O /tmp/docker/docker.bash $INSTALL_BASH_URL
-wget -O /tmp/docker/docker.tgz "https://download.docker.com/linux/static/stable/$DOCKER_ARCH/docker-$DOCKER_VERSION.tgz"
+cd /tmp/docker/
+wget $INSTALL_SH_URL
+wget $INSTALL_BASH_URL
+wget "https://download.docker.com/linux/static/stable/$DOCKER_ARCH/docker-$DOCKER_VERSION.tgz"
 chmod 777 -R /tmp/docker/
