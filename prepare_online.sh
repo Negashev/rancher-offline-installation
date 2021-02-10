595e98d50e01898026bd004dc87008548a1bf3d9
Negash <i@negash.ru>
2021-02-11 04:30:41 +0500

    Run chartmuseum and bastion-static

--- prepare_online.sh
+++ prepare_online.sh
@@ -38,6 +38,8 @@ echo "remove dublicate from offline-images.txt"
 sort /tmp/rancher/offline-images.txt | uniq -u | tee /tmp/rancher/offline.txt
 
 echo "install docker"
+
+echo '{ "log-opts": { "max-size": "10m", "max-file": "2" }, "insecure-registries" : ["'$BASTION_HOST':5000"] }' > /tmp/docker/daemon.json
 sh /get_docker.sh
 docker build --build-arg DOCKER_VERSION=$DOCKER_VERSION -t bastion-static -f /Dockerfile.bastion /tmp/docker
 
@@ -53,7 +55,6 @@ else
       bash /tmp/rancher/rancher-save-images.sh --image-list /tmp/rancher/offline.txt --images /tmp/rancher/rancher-images.tar.gz
 fi
 
-echo '{ "log-opts": { "max-size": "10m", "max-file": "2" }, "insecure-registries" : ["'$BASTION_HOST':5000"] }' > /tmp/daemon.json
 
 if test -z "$upload_to_bastion"
 then
@@ -100,14 +101,9 @@ else
       echo $BASTION_SCP | sed -r 's/\{source\}/\/tmp\/docker/g' | sed -r 's|\{destination\}|'$BASTION_DIR'\/docker|g' | sed -r 's|\{host\}|'$BASTION_HOST'|g' | sed -r 's|\{user\}|'$BASTION_USER'|g' > $temp_file
       sh $temp_file
       rm $temp_file
-      echo "scp /tmp/daemon.json to bastion"
-      temp_file=$(mktemp)
-      echo $BASTION_SCP | sed -r 's/\{source\}/\/tmp\/daemon.json/g' | sed -r 's|\{destination\}|'$BASTION_DIR'\/daemon.json|g' | sed -r 's|\{host\}|'$BASTION_HOST'|g' | sed -r 's|\{user\}|'$BASTION_USER'|g' > $temp_file
-      sh $temp_file
-      rm $temp_file
       echo "move $BASTION_DIR/daemon.json to /etc/docker/daemon.json on bastion"
       temp_file=$(mktemp)
-      echo "$BASTION_SSH_RUN sudo -S mv $BASTION_DIR/daemon.json /etc/docker/daemon.json" | sed -r 's|\{host\}|'$BASTION_HOST'|g' | sed -r 's|\{user\}|'$BASTION_USER'|g' > $temp_file
+      echo "$BASTION_SSH_RUN sudo -S mv $BASTION_DIR/docker/daemon.json /etc/docker/daemon.json" | sed -r 's|\{host\}|'$BASTION_HOST'|g' | sed -r 's|\{user\}|'$BASTION_USER'|g' > $temp_file
       sh $temp_file
       rm $temp_file
       echo ""
@@ -132,13 +128,25 @@ else
       echo ""
       echo "Run registry on bastion"
       temp_file=$(mktemp)
-      echo "$BASTION_SSH_RUN sudo -S docker run -d -p 5000:5000 --restart=always --name registry -v /var/lib/registry:/var/lib/registry registry:2" | sed -r 's|\{host\}|'$BASTION_HOST'|g' | sed -r 's|\{user\}|'$BASTION_USER'|g' > $temp_file
+      echo "$BASTION_SSH_RUN sudo -S docker run -dit -p 5000:5000 --restart=always --name registry -v /var/lib/registry:/var/lib/registry registry:2" | sed -r 's|\{host\}|'$BASTION_HOST'|g' | sed -r 's|\{user\}|'$BASTION_USER'|g' > $temp_file
       sh $temp_file
       rm $temp_file
       echo ""
       echo "load offline images to registry on bastion"
       temp_file=$(mktemp)
       echo "$BASTION_SSH_RUN sudo -S bash $BASTION_DIR/rancher-load-images.sh --images $BASTION_DIR/rancher-images.tar.gz --image-list $BASTION_DIR/rancher-images.txt  --image-list $BASTION_DIR/rancher-images.txt --registry $BASTION_HOST:5000" | sed -r 's|\{host\}|'$BASTION_HOST'|g' | sed -r 's|\{user\}|'$BASTION_USER'|g' > $temp_file
+      sh $temp_file
+      rm $temp_file
+      echo ""
+      echo "Run chartmuseum on bastion"
+      temp_file=$(mktemp)
+      echo "$BASTION_SSH_RUN sudo -S docker run -dit -p 8080:8080 --restart=always --name chartmuseum -v $BASTION_DIR/charts:/charts -e DEBUG=true -e STORAGE=local -e STORAGE_LOCAL_ROOTDIR=/charts $BASTION_HOST:5000/chartmuseum/chartmuseum:$CHARTMUSEAM_VERSION" | sed -r 's|\{host\}|'$BASTION_HOST'|g' | sed -r 's|\{user\}|'$BASTION_USER'|g' > $temp_file
+      sh $temp_file
+      rm $temp_file
+      echo ""
+      echo "Run bastion-static on bastion"
+      temp_file=$(mktemp)
+      echo "$BASTION_SSH_RUN sudo -S docker run -dit -p 80:80 --restart=always --name bastion-static $BASTION_HOST:5000/bastion-static" | sed -r 's|\{host\}|'$BASTION_HOST'|g' | sed -r 's|\{user\}|'$BASTION_USER'|g' > $temp_file
       sh $temp_file
       rm $temp_file
       echo ""
