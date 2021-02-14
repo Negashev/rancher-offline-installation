814b6cc86d4bd4a3cdafa8a31b9a6a3695162b23
Negash <i@negash.ru>
2021-02-15 04:47:28 +0500

    One cmd terragrunt run-all init

--- get_terragrunt.sh
+++ get_terragrunt.sh
@@ -6,12 +6,10 @@ cp -r /terragrunt/* /tmp/terragrunt/
 
 cd /tmp/terragrunt/
 
+echo "Clean terragrunt"
 docker kill terragrunt || true
 docker rm terragrunt || true
 
 
-docker run -dit --name terragrunt -v $HOST_MOUNT/terragrunt:/root/terragrunt -w /root/terragrunt --entrypoint=sh alpine/terragrunt
-
-# download dependency
-docker exec -it terragrunt terragrunt run-all init
+docker run -dit --name terragrunt -v $HOST_MOUNT/terragrunt:/root/terragrunt -w /root/terragrunt alpine/terragrunt run-all init
 
