a8db855be54419167771dea45a424fc3d7df2876
Negash <i@negash.ru>
2021-03-02 20:31:38 +0500

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -4,5 +4,6 @@ wget $BASTION_HOST/install.sh
 wget $BASTION_HOST/docker.bash
 wget $BASTION_HOST/daemon.json
 wget $BASTION_HOST/docker-$DOCKER_VERSION.tgz
+sudo mkdir -p /etc/docker
 sudo cp /tmp/daemon.json /etc/docker/daemon.json
 bash /tmp/install.sh -f /tmp/docker-$DOCKER_VERSION.tgz
