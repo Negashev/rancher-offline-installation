fae6cce49322384692764518e39617003c24949d
Negash <i@negash.ru>
2021-03-07 23:08:25 +0500

    Update rancher.tf

--- terragrunt/3_install_rancher/rancher.tf
+++ terragrunt/3_install_rancher/rancher.tf
@@ -39,9 +39,5 @@ resource "helm_release" "rancher" {
     name  = "rancherImageTag"
     value = var.rancher_image_tag
   }
-  
-  set {
-    name  = "certmanager.version"
-    value = var.cert_manager_version
-  }  
+ 
 }
