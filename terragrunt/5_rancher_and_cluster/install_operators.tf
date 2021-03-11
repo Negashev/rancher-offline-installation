cf69b5b8354bf34ce875f294b680f8f20ce1f50a
Negash <i@negash.ru>
2021-03-11 19:15:02 +0500

    Update install_operators.tf

--- terragrunt/5_rancher_and_cluster/install_operators.tf
+++ terragrunt/5_rancher_and_cluster/install_operators.tf
@@ -70,7 +70,7 @@ resource "rancher2_app" "database" {
   template_version = var.postgres_operator_version
   target_namespace = rancher2_namespace.database.id
   answers = {
-    "image.registry" = "${var.bastion_host}:5000/registry.opensource.zalan.do/acid/postgres-operator"
+    "image.registry" = "${var.bastion_host}:5000/registry.opensource.zalan.do"
   }
   depends_on = [
     rancher2_namespace.database
