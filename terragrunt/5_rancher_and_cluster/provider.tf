f9b55e5c3b830c0a1fb275d9ba439350279b7433
Negash <i@negash.ru>
2021-03-12 21:34:21 +0500

    Update provider.tf

--- terragrunt/5_rancher_and_cluster/provider.tf
+++ terragrunt/5_rancher_and_cluster/provider.tf
@@ -4,11 +4,6 @@ terraform {
       source = "rancher/rancher2"
       version = "1.12.0"
     }
-  }
-}
-
-terraform {
-  required_providers {
     time = {
       source = "hashicorp/time"
       version = "0.7.0"
