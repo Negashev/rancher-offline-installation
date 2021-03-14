260a9d89ad9ba4c134d1af23d722b42dbbef8fc4
Negash <i@negash.ru>
2021-03-14 16:33:14 +0500

    Update get_docker.sh

--- get_docker.sh
+++ get_docker.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 mkdir -p /tmp/docker/
 cd /tmp/docker/
-wget $INSTALL_SH_URL
-wget $INSTALL_BASH_URL
 wget "https://download.docker.com/linux/static/stable/$DOCKER_ARCH/docker-$DOCKER_VERSION.tgz"
 chmod 777 -R /tmp/docker/
