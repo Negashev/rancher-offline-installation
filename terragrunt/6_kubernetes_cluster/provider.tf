8fa7fc3a6750a2d649c5a0c4a696f3c1ba40d3d7
Negash <i@negash.ru>
2021-03-22 01:14:55 +0500

    Update provider.tf

--- terragrunt/6_kubernetes_cluster/provider.tf
+++ terragrunt/6_kubernetes_cluster/provider.tf
@@ -1,3 +1,9 @@
+# for running kubectl
+resource "local_file" kubeconfig {
+  filename = "${path.module}/kubeconfig"
+  content  = var.kube_config_content
+}
+
 provider "kubernetes-alpha" {
-  config_path = "/mount/kubeconfig" // path to kubeconfig
+  config_path = "${path.module}/kubeconfig" // path to kubeconfig
 }
