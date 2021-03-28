46fc086f3b33740236116f360099f8de6cc0bb12
Negash <i@negash.ru>
2021-03-29 00:07:26 +0500

    Update install_docker.tf

--- terragrunt/5_rancher_and_cluster/install_docker.tf
+++ terragrunt/5_rancher_and_cluster/install_docker.tf
@@ -24,13 +24,13 @@ resource "null_resource" "install_docker_brain" {
 }
 
 resource "null_resource" "install_docker_storage" {    
-    for_each = toset(keys(var.cluster_nodes.storage))
+    for_each = toset(var.cluster_nodes.storage)
     # setup docker on rancher nodes
     provisioner "remote-exec" {
         inline = [
             "echo ${var.ssh_password} | sudo -S usermod -aG root ${var.ssh_user}",
             "echo ${var.ssh_password} | sudo -S curl ${var.bastion_host}/install_docker.sh | sudo -S BASTION_HOST=${var.bastion_host} DOCKER_VERSION=${var.docker_version} bash - ",
-            "echo ${var.ssh_password} | ${rancher2_cluster.cluster.cluster_registration_token.0.node_command} --worker --label app=storage --taints storage=services:NoSchedule --internal-address ${each.key} --address ${var.cluster_nodes.storage[each.key]}",
+            "echo ${var.ssh_password} | ${rancher2_cluster.cluster.cluster_registration_token.0.node_command} --worker --label app=storage --taints storage=services:NoSchedule --internal-address ${each.key} --address ${each.key}",
         ]
 
         connection {
