68997e5618e46d8b7e5fc7639b9a3b324374259e
Negash <i@negash.ru>
2021-04-15 20:25:32 +0500

    Update get_helm.sh

--- get_helm.sh
+++ get_helm.sh
@@ -8,6 +8,7 @@ docker run --rm -it --name helm -v $HOST_MOUNT/helm:/root/ -w /root/ alpine/helm
 docker run --rm -it --name helm -v $HOST_MOUNT/helm:/root/ -w /root/ alpine/helm:$HELM_VERSION repo add rancher-stable https://releases.rancher.com/server-charts/stable
 docker run --rm -it --name helm -v $HOST_MOUNT/helm:/root/ -w /root/ alpine/helm:$HELM_VERSION repo add rook-release https://charts.rook.io/release
 docker run --rm -it --name helm -v $HOST_MOUNT/helm:/root/ -w /root/ alpine/helm:$HELM_VERSION repo add nvidia https://nvidia.github.io/k8s-device-plugin
+docker run --rm -it --name helm -v $HOST_MOUNT/helm:/root/ -w /root/ alpine/helm:$HELM_VERSION repo add bitnami https://charts.bitnami.com/bitnami
 docker run --rm -it --name helm -v $HOST_MOUNT/helm:/root/ -w /root/ alpine/helm:$HELM_VERSION repo add postgres-operator "https://raw.githubusercontent.com/zalando/postgres-operator/$POSTGRES_OPERATOR_VERSION/charts/postgres-operator/"
 docker run --rm -it --name helm -v $HOST_MOUNT/helm:/root/ -w /root/ alpine/helm:$HELM_VERSION repo update
 
@@ -16,9 +17,10 @@ docker run --rm -it --name helm -v $HOST_MOUNT/helm:/root/ -w /root/ alpine/helm
 docker run --rm -it --name helm -v $HOST_MOUNT/helm:/root/ -w /root/ alpine/helm:$HELM_VERSION fetch rancher-stable/rancher --version $RANCHER_VERSION
 docker run --rm -it --name helm -v $HOST_MOUNT/helm:/root/ -w /root/ alpine/helm:$HELM_VERSION fetch rook-release/rook-ceph --version $ROOK_VERSION
 docker run --rm -it --name helm -v $HOST_MOUNT/helm:/root/ -w /root/ alpine/helm:$HELM_VERSION fetch nvidia/nvidia-device-plugin --version $NVIDIA_VERSION
+docker run --rm -it --name helm -v $HOST_MOUNT/helm:/root/ -w /root/ alpine/helm:$HELM_VERSION fetch bitnami/metallb --version $METALLLB_VERSION
 docker run --rm -it --name helm -v $HOST_MOUNT/helm:/root/ -w /root/ alpine/helm:$HELM_VERSION fetch postgres-operator/postgres-operator --version $POSTGRES_OPERATOR_VERSION
 
-echo "save images from rancher-$RANCHER_VERSION.tgz, cert-manager-$CERT_MANAGER_VERSION.tgz, nvidia-device-plugin-$NVIDIA_VERSION.tgz, postgres-operator-$POSTGRES_OPERATOR_VERSION.tgz and rook-ceph-$ROOK_VERSION.tgz"
+echo "save images"
 for i in `ls /tmp/helm`; do docker run --rm -it --name helm -v $HOST_MOUNT/helm:/root/ -w /root/ alpine/helm:$HELM_VERSION template ./$i | grep -oP '(?<=image: ).*(?=)' | tr -d '"' >> /tmp/rancher/offline-images.txt; done
 echo 'save csi images for rook'
 tar -axf /tmp/helm/rook-ceph-*.tgz rook-ceph/values.yaml -O | grep -oP '(?<=image: ).*(?=)' | tr -d '"' >> /tmp/rancher/offline-images.txt
