4b245602f213f3570a63bd85c22b56a156974d14
Negash <i@negash.ru>
2021-04-21 22:22:34 +0500

    Create variables.tf

---
+++ terragrunt/8_metallb/variables.tf
@@ -0,0 +1,13 @@
+variable "api_url" {}
+variable "token_key" {}
+variable "system_project_id" {}
+
+variable "bastion_host" {
+  type = string
+  default = "0.0.0.0"
+}
+
+variable "metallb_addresses" {
+  type = string
+  default = "0.0.0.10-0.0.0.50"
+}
