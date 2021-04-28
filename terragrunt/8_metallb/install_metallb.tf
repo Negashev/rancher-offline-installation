8ea0721a28ce5845bbcf254db7efbfefa42dbb05
Negash <i@negash.ru>
2021-04-29 01:36:07 +0500

    Update install_metallb.tf

--- terragrunt/8_metallb/install_metallb.tf
+++ terragrunt/8_metallb/install_metallb.tf
@@ -21,3 +21,29 @@ resource "rancher2_app" "metallb" {
     rancher2_namespace.metallb
   ]
 }
+
+resource "rancher2_namespace" "ingress" {
+  name = "ingress-nginx"
+  project_id = var.system_project_id
+}
+
+
+resource "rancher2_app" "ingress" {
+  catalog_name = "bastion"
+  name = "ingress-nginx"
+  project_id = var.system_project_id
+  template_name = "ingress-nginx"
+  template_version = var.ingress_version
+  target_namespace = rancher2_namespace.ingress.id
+  wait = true
+  answers = {
+    "controller.image.digest" = ""
+    "controller.image.repository" = "${var.bastion_host}:5000/k8s.gcr.io/ingress-nginx/controller"
+    "controller.affinity.podAntiAffinity.requiredDuringSchedulingIgnoredDuringExecution[0].topologyKey" = "kubernetes.io/hostname"
+    "controller.replicaCount" = "2"
+  }
+  depends_on = [
+    rancher2_namespace.ingress,
+    rancher2_app.metallb
+  ]
+}
